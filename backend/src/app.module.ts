a8ee4731f20dbf25cb3b7758e213f682f23b4cf6
YogyerekDEV <mszebi2008@gmail.com>
2026-02-03 19:40:38 +0100

    (OC-01): generated prisma, movies, show files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { MoviesModule } from './movies/movies.module';
+import { ShowModule } from './show/show.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, MoviesModule, ShowModule],
   controllers: [AppController],
   providers: [AppService],
 })
